f77214d27a0bf7074feb2468e861128eed5ab2ad
dendeaisd <flavia.voicuu@gmail.com>
2024-04-13 07:58:12 +0200

    ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fvoicu <fvoicu@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/13 07:35:01 by fvoicu            #+#    #+#             */
+/*   Updated: 2024/04/13 07:57:23 by fvoicu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::string str = "HI THIS IS BRAIN";
+  std::string* stringPTR = &str;
+  std::string& stringREF = str;
+
+  std::cout << "Address of string variable: " << &str << std::endl;
+  std::cout << "Adress held by stringPTR: " << stringPTR << std::endl;
+  std::cout << "Adress held by stringREF: " << &stringREF << std::endl;
+
+  std::cout << "Value of the string variable: " << str << std::endl;
+  std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+  std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+}
