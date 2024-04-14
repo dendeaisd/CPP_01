c48d2862c6682692ac058e2f6c269b9879dbda6b
dendeaisd <flavia.voicuu@gmail.com>
2024-04-14 11:57:54 +0200

    fixed some logic issue/ wrong files + completed exercise

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,49 +6,30 @@
 /*   By: fvoicu <fvoicu@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 11:24:01 by fvoicu            #+#    #+#             */
-/*   Updated: 2024/04/14 11:48:40 by fvoicu           ###   ########.fr       */
+/*   Updated: 2024/04/14 11:56:12 by fvoicu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-Harl::Harl(){}
-
-Harl::~Harl(){}
-
-void Harl::debug(void) {
-  std::cout << "I love having extra baconfor my ";
-  std::cout << "7XL-double-cheese-triple-pickle-special";
-  std::cout << "-ketchup burger. I really do!" << std::endl;
-}
-
-void Harl::info(void) {
-  std::cout << "I cannot believe adding extra bacon costs more money.";
-  std::cout << " You didn’t put enough bacon in my burger!";
-  std::cout << "  If you did, I wouldn’t be asking for more!" << std::endl;
-}
-
-void Harl::warning(void) {
-  std::cout << "I think I deserve to have some extra bacon for free.";
-  std::cout << " I’ve been coming for years whereas you started";
-  std::cout << " working here since last month." << std::endl;
-}
-
-void Harl::error(void) {
-  std::cout << "This is unacceptable!";
-  std::cout << " I want to speak to the manager now." << std::endl; 
-}
-
-void Harl::complain(std::string level)
+int main()
 {
-  std::string lvl[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-  void (Harl::*fun[4])(void) = {Harl::debug, Harl::info, \
-                              Harl::warning, Harl::error};
-  for(int i = -1; i < 4; ++i) {
-    if (level.compare(lvl[i])) {
-      (this->*fun[i])();
-      return ;
-    }
-  }
-  std::cerr << "Level unknown." << std::endl;
+  Harl harl;
+   
+  std::cout << "Testing DEBUG level:" << std::endl;
+  harl.complain("DEBUG");
+
+  std::cout << "\nTesting INFO level:" << std::endl;
+  harl.complain("INFO");
+
+  std::cout << "\nTesting WARNING level:" << std::endl;
+  harl.complain("WARNING");
+
+  std::cout << "\nTesting ERROR level:" << std::endl;
+  harl.complain("ERROR");
+
+  std::cout << "\nTesting UNKNOWN level:" << std::endl;
+  harl.complain("UNKNOWN");
+
+  return 0; 
 }
