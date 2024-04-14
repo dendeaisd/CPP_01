7823421c273259ebf6b4e835855bd9b7d58e4b0b
dendeaisd <flavia.voicuu@gmail.com>
2024-04-14 10:54:15 +0200

    it worksss

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fvoicu <fvoicu@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 06:14:18 by fvoicu            #+#    #+#             */
-/*   Updated: 2024/04/14 08:42:42 by fvoicu           ###   ########.fr       */
+/*   Updated: 2024/04/14 10:47:16 by fvoicu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,50 @@
 
 void replaceString(std::string& source, \
       const std::string& from, const std::string& to) {
-  
+  std::string result;
+  std::size_t startPos = 0;
+  std::size_t findPos;
+
+  if (from.empty()) return;
+  result.reserve(source.length()); //performance, idk if its worth it here 
+  while((findPos = source.find(from, startPos)) != std::string::npos)
+  {
+    result.append(source, startPos, findPos - startPos); //append part before from
+    result.append(to);// append 'to'
+    startPos = findPos + from.length();// move past initial 'from' part
+  }
+  result.append(source, startPos, std::string::npos); //apend rest of the string
+  source.swap(result); //replace the contents of source
 }
 
 int main(int ac, char **av) {
-  if (ac == 3) {
-    std::ifstream inFile(av[0]);
-    if (!inFile)
-      return (std::cerr << "Failed to open file." << std::endl, 1);
-    
-    
-    inFile.close();
+  if (ac != 4)
+    return (std::cerr << "Usage: " << av[0] << \
+    " <filename> <s1> <s2>" << std::endl, 1);
+
+  std::string filename(av[1]);
+  std::string s1(av[2]);
+  std::string s2(av[3]);    
+  
+  std::ifstream inFile(filename);
+  if (!inFile)
+    return (std::cerr << "Failed to open  the file: " << \
+              filename << std::endl, 1);
+              
+  std::string newFilename = filename + ".replace";
+  std::ofstream outFile(newFilename);
+  if (!outFile)
+    return (std::cerr << "Failed to create the file: " << \
+              filename << std::endl, 1); 
+  
+  std::string line;
+  while (getline(inFile, line)) {
+    replaceString(line, s1, s2);
+    outFile << line << "\n";
   }
+
+  inFile.close();
+  outFile.close();
+  std::cout << "File processing complete. Output file: " << \
+              newFilename << std::endl;
 }
